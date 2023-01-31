982ed5a3c11c07aa47d551ad449242a80b3a2b15
imranhub10 <alimrancse100@gmail.com>
2023-01-31 19:41:12 +0600

    calculator

---
+++ marks_calculator/calculator.php
@@ -0,0 +1,21 @@
+<?php
+     $marks = 95;
+
+     if($marks >=80 && $marks <=100){
+         echo "You got A+";
+     }elseif($marks >=70 && $marks <=79){
+         echo "You got A";
+     }elseif($marks >=60 && $marks <=69){
+         echo "You got A-";
+     }elseif($marks >=50 && $marks <=59){
+         echo "You got B";
+     }elseif($marks >=40 && $marks <=49){
+         echo "You got C";
+     }elseif($marks >=33 && $marks <=39){
+         echo "You got D";
+     }elseif($marks >=0 && $marks <=32){
+         echo "You got F";
+     }else{
+         echo "Invalid Number";
+     }
+?>
